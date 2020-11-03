4510a336ddd90e4702c4926a7f0940709ff9dd36
Prashanth Puranik <puranik@digdeeper.in>
2020-11-03 23:38:39 +0530

    bug fix

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -27,7 +27,8 @@ import javax.validation.constraints.NotEmpty;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-	@Column(name = "first_name")
+    @Column(name = "first_name")
+    @NotEmpty
 	private String firstName;
 
 	@Column(name = "last_name")
